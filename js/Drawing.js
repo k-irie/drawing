190245f39f7a5272d826476027974d284b15c08a
hiramatsu <kkermitjp@gmail.com>
2024-11-28 09:24:47 +0900

    2024/11/27 3コマ目終了時

--- js/Drawing.js
+++ js/Drawing.js
@@ -11,6 +11,16 @@ var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (
     return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
 var _Drawing_width, _Drawing_height;
+// arcメソッド用のパラメータクラス
+//  パラメータをクラス化する
+class ArcParams {
+    constructor() {
+        this.f = 2; // 描画方法 1:塗り潰し、2:枠線:、3:塗り潰し+枠線
+        this.sd = 0; // 開始角度 既定値:0度
+        this.ed = 360; // 終了角度 既定値:360度
+        this.last = true; // 最後のlineToメソッドを実行するか true:実行する、false:実行しない
+    }
+}
 class Drawing {
     //  アクセサメソッドを使い直接外部からアクセス出来ないようにする
     get width() {
@@ -30,6 +40,10 @@ class Drawing {
             __classPrivateFieldSet(this, _Drawing_height, this.canvas.height = h, "f");
         }
     }
+    // 全画面消去
+    clearAll() {
+        this.context?.clearRect(0, 0, this.width, this.height);
+    }
     /**
      *
      * @param x 左上の座標
@@ -45,6 +59,39 @@ class Drawing {
         if (f & 2) { // 枠
             this.context?.strokeRect(x, y, w, h);
         }
+    }
+    /**
+     *
+     * @param x 左上の座標
+     * @param y 左上の座標
+     * @param r 半径
+     * @param p 追加パラメータ
+     * // @param f 描画方法 1:塗り潰し、2:枠線、3:塗り潰し+枠線
+     * // @param sr 円弧の開始角度(度)
+     * // @param er 円弧の終了角度(度)
+     */
+    // arc(x: number, y: number, r: number, f: number = 2, sd: number = 0, ed: number = 360) {
+    arc(x, y, r, p = new ArcParams()) {
+        // 引数の値を元に左上の座標を計算する
+        const cx = x + r;
+        const cy = y + r;
+        const sr = p.sd * Math.PI / 180;
+        const er = p.ed * Math.PI / 180;
+        // 円弧は線を引くためのパスを定義しそれに対して実際に線を引く
+        //  パスを引くモードに切り替える
+        this.context?.beginPath();
+        //  円弧を描くようにパスを設定する
+        this.context?.arc(cx, cy, r, sr, er);
+        //  円弧の最後の点から中心点へパスを割り当てる
+        if (p.last) {
+            this.context?.lineTo(cx, cy);
+        }
+        //  円弧を塗りつぶす
+        this.context?.fill();
+        //  円弧に枠線を引く
+        this.context?.stroke();
+        //  パスモードを終了する
+        this.context?.closePath();
     }
     /**
      *
--- src/Drawing.ts
+++ src/Drawing.ts
@@ -1,4 +1,11 @@
-
+// arcメソッド用のパラメータクラス
+//  パラメータをクラス化する
+class ArcParams {
+    f = 2           // 描画方法 1:塗り潰し、2:枠線:、3:塗り潰し+枠線
+    sd = 0          // 開始角度 既定値:0度
+    ed = 360        // 終了角度 既定値:360度
+    last = true     // 最後のlineToメソッドを実行するか true:実行する、false:実行しない
+}
 
 class Drawing {
     // 幅と高さをプロパティとして持つことにする
@@ -29,6 +36,11 @@ class Drawing {
     // 上の2D描画エンジン
     context: CanvasRenderingContext2D | null = null
 
+    // 全画面消去
+    clearAll() {
+        this.context?.clearRect(0, 0, this.width, this.height)
+    }
+
     /**
      * 
      * @param x 左上の座標
@@ -51,15 +63,32 @@ class Drawing {
      * @param x 左上の座標
      * @param y 左上の座標
      * @param r 半径
-     * @param sr 円弧の開始角度
-     * @param er 円弧の終了角度
-     * @param f 描画方法
+     * @param p 追加パラメータ
+     * // @param f 描画方法 1:塗り潰し、2:枠線、3:塗り潰し+枠線
+     * // @param sr 円弧の開始角度(度)
+     * // @param er 円弧の終了角度(度)
      */
-    arc(x: number, y: number, r: number, sr: number, er: number, f: number) {
-        // 
+    // arc(x: number, y: number, r: number, f: number = 2, sd: number = 0, ed: number = 360) {
+    arc(x: number, y: number, r: number, p: ArcParams = new ArcParams()) {
+        // 引数の値を元に左上の座標を計算する
+        const cx = x + r
+        const cy = y + r
+        const sr = p.sd * Math.PI / 180
+        const er = p.ed * Math.PI / 180
+        // 円弧は線を引くためのパスを定義しそれに対して実際に線を引く
+        //  パスを引くモードに切り替える
         this.context?.beginPath()
-        this.context?.arc(x, y, r, sr, er)
+        //  円弧を描くようにパスを設定する
+        this.context?.arc(cx, cy, r, sr, er)
+        //  円弧の最後の点から中心点へパスを割り当てる
+        if (p.last){
+            this.context?.lineTo(cx, cy)
+        }
+        //  円弧を塗りつぶす
+        this.context?.fill()
+        //  円弧に枠線を引く
         this.context?.stroke()
+        //  パスモードを終了する
         this.context?.closePath()
     }
 
